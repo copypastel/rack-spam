a889005dde34a7e87caded69b867aec205054ca8
ecin <ecin@copypastel.com>
2009-04-16 21:10:06 -0400

    Added specs for Spam#comment?, Spam#spam? should now expect an :env, not a :comment

--- specs/rack-spam_spec.rb
+++ specs/rack-spam_spec.rb
@@ -9,12 +9,27 @@ describe Rack::Spam do
   before :all do
     config = File.open('config.yaml') {|f| YAML::load(f) }
     @domain = config[:domain]
+    @akismet_key, @defensio_key, @mollom_key = [:akismet, :defensio, :mollom].map {|s| config[s]}
+    @env
+  end
+  
+  describe "#valid" do
+    
+    it "should check that :env is a Hash"
+    it "should check that the request is a POST"
+    it "should check against the POST URL"
+    it "should check for :username, :email, and :comment in the input stream"
+    
   end
   
   describe 'when using Akismet' do
     
     before :all do
-      @filter = Rack::Spam.new(:akismet)
+      @filter = Rack::Spam.new(:akismet, @domain, @akismet_key)
+    end
+    
+    it "should flag a comment" do
+      @filter.spam? @env
     end
     
   end
@@ -22,7 +37,11 @@ describe Rack::Spam do
   describe 'when using Defensio' do
     
     before :all do
-      @filter = Rack::Spam.new(:defensio)
+      @filter = Rack::Spam.new(:defensio, @domain, @defensio_key)
+    end
+    
+    it "should flag a comment" do
+      @filter.spam? @env
     end
     
   end
@@ -30,7 +49,11 @@ describe Rack::Spam do
   describe 'when using Mollom' do
     
     before :all do
-      @filter = Rack::Spam.new(:mollom)
+      @filter = Rack::Spam.new(:mollom, @domain, @mollom_key)
+    end
+    
+    it "should flag a comment" do
+      @filter.spam? @env
     end
     
   end
