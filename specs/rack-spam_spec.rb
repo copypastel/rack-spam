363fdb5024ce0fadf88991f9a6cc64a385ce559d
ecin <ecin@copypastel.com>
2009-04-16 23:23:14 -0400

    Further work on Spam#comment? and Spam#spam? specs.

--- specs/rack-spam_spec.rb
+++ specs/rack-spam_spec.rb
@@ -5,57 +5,90 @@ require File.expand_path(File.dirname(__FILE__) + '/../gem/ruby-mollom/lib/mollo
 
 
 describe Rack::Spam do
-  
+
   before :all do
     config = File.open('config.yaml') {|f| YAML::load(f) }
     @domain = config[:domain]
+    @post_url = config[:post_url]
     @akismet_key, @defensio_key, @mollom_key = [:akismet, :defensio, :mollom].map {|s| config[s]}
     @env
   end
-  
-  describe "#valid" do
-    
-    it "should check that :env is a Hash"
-    it "should check that the request is a POST"
-    it "should check against the POST URL"
-    it "should check for :username, :email, and :comment in the input stream"
-    
+
+  describe "#comment?" do
+
+    before :all do
+      @filter = Rack::Spam.new(:akismet, @domain, @akismet_key, @post_url)
+    end
+
+    before :each do
+      @env = {}
+    end
+
+    it "should accept @env as a valid comment" do
+      @filter.comment?(@env).should be(true)
+    end
+
+    it "should check that the request is a POST" do
+      @env[:REQUEST_METHOD] = 'GET'
+      @filter.comment?(@env).should be(false)
+    end
+
+    it "should check against the comments POST URL" do
+      @env[:PATH_INFO] = ''
+      @filter.comment?(@env).should be(false)
+    end
+
+    it "should check for :username, :email, and :comment values in the input stream"
+
   end
-  
+
   describe 'when using Akismet' do
-    
+
     before :all do
-      @filter = Rack::Spam.new(:akismet, @domain, @akismet_key)
+      @filter = Rack::Spam.new(:akismet, @domain, @akismet_key, @post_url)
+      @spam = {}
     end
-    
-    it "should flag a comment" do
-      @filter.spam? @env
+
+    describe '#spam?' do
+
+      it "should flag a comment as spam" do
+        @filter.spam?(@spam).should be(true)
+      end
+
     end
     
   end
-  
+
   describe 'when using Defensio' do
-    
+
     before :all do
-      @filter = Rack::Spam.new(:defensio, @domain, @defensio_key)
+      @filter = Rack::Spam.new(:defensio, @domain, @defensio_key, @post_url)
     end
-    
-    it "should flag a comment" do
-      @filter.spam? @env
+
+    describe '#spam?' do
+
+      it "should flag a comment as spam" do
+        @filter.spam?(@spam).should be(true)
+      end
+      
     end
     
   end
-  
+
   describe 'when using Mollom' do
-    
+
     before :all do
-      @filter = Rack::Spam.new(:mollom, @domain, @mollom_key)
+      @filter = Rack::Spam.new(:mollom, @domain, @mollom_key, @post_url)
     end
-    
-    it "should flag a comment" do
-      @filter.spam? @env
+
+    describe '#spam?' do
+
+      it "should flag a comment as spam" do
+        @filter.spam?(@spam).should be(true)
+      end
+
     end
     
   end
-  
+
 end
