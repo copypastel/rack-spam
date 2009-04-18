6dd597452358e32b5b3e9145dbece36c9591ba9e
ecin <ecin@copypastel.com>
2009-04-18 06:34:54 -0400

    Rack::Spam::Filter::Akismet#spam? now makes sures the parameters taken from :env are set as an empty string, should they be nil.

--- lib/filter/akismet.rb
+++ lib/filter/akismet.rb
@@ -1,11 +1,11 @@
+require 'rack'
+
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/spam')
 require File.expand_path(File.dirname(__FILE__) + '/../../lib/filter')
 require File.expand_path(File.dirname(__FILE__) + '/../../gem/akismet/lib/akismet')
 
 module Rack::Spam::Filter
   class Akismet < ::Akismet
-
-    include Rack::Spam::Filter
 
     def initialize(domain, key, post_url)
       @post_url = post_url
@@ -26,9 +26,9 @@ module Rack::Spam::Filter
     end
 
     def spam?( env )
-      r = Request.new env
-      params = { :user_ip => r.ip, 
-                  :user_agent => env['HTTP_USER_AGENT'], 
+      r = Rack::Request.new env
+      params = { :user_ip => r.ip || '', 
+                  :user_agent => env['HTTP_USER_AGENT'] || '', 
                   :referrer => r.referrer, 
                   :comment_author => r['username'], 
                   :comment_author_email => r['email'], 
