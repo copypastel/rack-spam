6c7e5a3576edbaf444df351dbbd8e8c0b6786657
ecin <ecin@copypastel.com>
2009-04-18 01:43:43 -0400

    Moving most functionality away from Rack::Spam and into Rack::Spam::Filter and individual filters for each spam webservice, i.e. Akismet.

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,47 +1,30 @@
-require File.expand_path(File.dirname(__FILE__) + '/../gem/akismet/lib/akismet')
 require File.expand_path(File.dirname(__FILE__) + '/../gem/rdefensio/lib/rdefensio')
 require File.expand_path(File.dirname(__FILE__) + '/../gem/ruby-mollom/lib/mollom')
 
 module Rack
   module Spam
-    class Filter
+    module Filter
       
       attr_reader :domain, :key, :post_url
       
-      class Service < Struct.new(:name, :instance); end;
-      
-      @@services = [:akismet, :defensio, :mollom]
-      
-      def initialize(service, domain, key, post_url)
-        raise InvalidArgument unless @@services.include? service
-        @domain, @key, @post_url = domain, key, post_url
-        case service
-          when :akismet: @service = Service.new(service, Akismet.new(key, domain))
-          when :defensio:
-          when :mollom:
-        end
+      def self.build(service, domain, key, post_url)
+        self.const_get(service.to_s.capitalize).new(domain, key, post_url)
       end
       
       def spam?( env )
-        true
+        raise NotImplementedError
       end
 
       def comment?( env )
-        return false unless env['REQUEST_METHOD'] == 'POST'
-        return false unless env['PATH_INFO'] =~ Regexp.new('.*' + @post_url)
-        input = env['rack.input'].read
-        [/&?username=/, /&?email=/, /&?comment=/].each do |attribute|
-          return false unless input =~ attribute
-        end
-        true
+        raise NotImplementedError
       end
       
       def verify?
-        @service.instance.verify?
+        raise NotImplementedError
       end
       
       def service
-        @service.name
+        raise NotImplementedError
       end
       
     end
