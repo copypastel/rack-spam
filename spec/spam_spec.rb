4ebef2cb1883edbc5616594f222b2da1534f4682
ecin <ecin@copypastel.com>
2009-05-07 18:02:20 -0400

    Added comments to better explain specs.

--- spec/spam_spec.rb
+++ spec/spam_spec.rb
@@ -16,13 +16,16 @@ describe Rack::Spam do
     @key = 'e56eb5d6b47c'
     @post_url = '/comments'
     @env = Rack::MockRequest.env_for('/comments', {:input => "username=ecin&email=ecin@copypastel.com&comment=This is awesome!"})
+    # The Lambdacat App
     @app = lambda { |env| [200, {}, "Lambda, lambda, lambda app, hoooo!"] }
   end
 
+  # Rack::Spam middleware can be instantiated
   it 'should accept an :app and an :opts hash at instantiation' do
     lambda { Spam.new(@app, {}) }.should_not raise_error
   end
   
+  # :opts defines the settings for Rack::Spam
   it 'should set filters given their parameters in the :opts hash' do
     middleware = Spam.new(@app, :domain => 'http://copypastel.com', 
                                 :post_url => '/comments', 
@@ -34,6 +37,7 @@ describe Rack::Spam do
     @middleware = Spam.new @app
   end
   
+  # Rack::Spam#call is defined, as in any middleware
   it 'should return an array with status, headers, and body when sent :call' do
     @middleware.add_filter(@service, @domain, @key, @post_url)
     response = @middleware.call(@env)
@@ -42,16 +46,19 @@ describe Rack::Spam do
     response[2] == "Lambda, lambda, lambda app, hoooo!"
   end
 
+  # Rack::Spam can't do its work without any filters to do the spam checking
   it 'should raise an error when checking for spam if no filters have been added' do
     @middleware.filters.should be_empty
     lambda { @middleware.spam? @env }.should raise_error
   end
 
+  # We can fix the empty filter set problem
   it 'should be capable of adding filters' do
     @middleware.add_filter(@service, @domain, @key, @post_url)
     @middleware.filters.size.should == 1
   end
   
+  # Each request is wrung through all the filters
   it 'should check against all filters when checking for spam' do
     @middleware.add_filter(@service, @domain, @key, @post_url)
     @middleware.filters.each {|f| f.should_receive :spam?}
