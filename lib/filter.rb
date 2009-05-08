bfbba58fda3a807d5e232a93e4e30d34869263e1
ecin <ecin@copypastel.com>
2009-05-08 12:16:35 -0400

    Somehow missed adding lib/filter.rb two commits ago. *sigh*

--- lib/filter.rb
+++ lib/filter.rb
@@ -6,13 +6,6 @@ class Rack::Spam
     def self.build(service, domain, key, post_url)
       self.const_get(service.to_s.capitalize).new(domain, key, post_url)
     end
-    
-    def comment?( env )
-      return false unless env['REQUEST_METHOD'] == 'POST' and 
-                          env['PATH_INFO'] =~ /#{@post_url}/
-      request = Request.new env
-      [ :username=, :email, :comment ].all? { |key| not request[key].nil?  }
-    end
 
   end
 end
