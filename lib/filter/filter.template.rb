b01b71e28eb0b6c836456c62f7f94367226239f1
ecin <ecin@copypastel.com>
2009-10-21 17:40:00 -0600

    Removed #comment? from filter.template, ironically added comments for other methods.

--- lib/filter/filter.template.rb
+++ lib/filter/filter.template.rb
@@ -9,9 +9,16 @@ module Rack::Spam::Filter
     end
 
     # Interface to be implemented by filters
+    
+    # Method to submit an env to the service and check if it's spam
+    # Returns true if env is spam, false otherwise
     def spam?( env );     raise NotImplementedError; end
-    def comment?( env );  raise NotImplementedError; end
+    
+    # Method to 'login' to the service. Verifies that the service credentials are valid.
+    # Returns true if credentials are valid. Should raise an exception otherwise
     def verify?;          raise NotImplementedError; end
+    
+    # Should return the service's name as a symbol, i.e. :akismet, :dfensio, :mollom, etc.
     def service;          raise NotImplementedError; end
 
   end
